1344acd3e2a0ebf3f6eee102fee9587eda4a5721
Joel Salisbury <joel.salisbury@gmail.com>
2023-12-20 17:57:57 -0500

    iframe height tweaks

--- public/index.php
+++ public/index.php
@@ -10,7 +10,8 @@ require_once '../inc/functions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pandemic Journaling Project (Test)</title>
     <!-- Bootstrap 5 CSS CDN -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
@@ -20,14 +21,16 @@ require_once '../inc/functions.php';
         <div class="row">
             <div class="col-md-12 mb-3">
                 <h2 class="mb-3 mt-3">Featured Entries</h2>
-                <p>Since May 2020, the Pandemic Journaling Project has given ordinary people a place to chronicle and preserve their pandemic experiences. In our first phase, PJP-1,
-                    more than 1,800 people in 55 countries have created nearly 27,000 individual journal entries—for themselves, and for the history books. </p>
+                <p>Since May 2020, the Pandemic Journaling Project has given ordinary people a place to chronicle and
+                    preserve their pandemic experiences. In our first phase, PJP-1,
+                    more than 1,800 people in 55 countries have created nearly 27,000 individual journal entries—for
+                    themselves, and for the history books. </p>
             </div>
         </div>
 
         <?php
         // Get the total number of entries
-        $totalEntries = getTotalEntries($_GET['type']   ?? null);
+        $totalEntries = getTotalEntries($_GET['type'] ?? null);
         // Calculate the total number of pages
         $totalPages = ceil($totalEntries / 48);
         if (isset($_GET['page'])) {
@@ -51,21 +54,29 @@ require_once '../inc/functions.php';
                         <div class="mb-3">
                             <h5 class="form-label mb-3" id="formatlabel">Format:</h5>
                             <div class="form-check">
-                                <input <?php if (isset($_GET['type']) && $_GET['type'] == "text_only") echo "checked"; ?> class="form-check-input" type="checkbox" name="type" value="text_only" id="text-only">
+                                <input <?php if (isset($_GET['type']) && $_GET['type'] == "text_only")
+                                    echo "checked"; ?>
+                                    class="form-check-input" type="checkbox" name="type" value="text_only"
+                                    id="text-only">
                                 <label class="form-check-label" for="text-only">
                                     Text Only
                                 </label>
                             </div>
 
                             <div class="form-check">
-                                <input <?php if (isset($_GET['type']) && $_GET['type'] == "photo_and_text") echo "checked"; ?> class="form-check-input" type="checkbox" name="type" value="photo_and_text" id="text-and-image">
+                                <input <?php if (isset($_GET['type']) && $_GET['type'] == "photo_and_text")
+                                    echo "checked"; ?> class="form-check-input" type="checkbox" name="type"
+                                    value="photo_and_text" id="text-and-image">
                                 <label class="form-check-label" for="text-and-image">
                                     Text & Image
                                 </label>
                             </div>
 
                             <div class="form-check">
-                                <input <?php if (isset($_GET['type']) && $_GET['type'] == "audio_only") echo "checked"; ?> class="form-check-input" type="checkbox" name="type" value="audio_only" id="text-and-audio">
+                                <input <?php if (isset($_GET['type']) && $_GET['type'] == "audio_only")
+                                    echo "checked"; ?>
+                                    class="form-check-input" type="checkbox" name="type" value="audio_only"
+                                    id="text-and-audio">
                                 <label class="form-check-label" for="text-and-audio">
                                     Text & Audio
                                 </label>
@@ -78,8 +89,13 @@ require_once '../inc/functions.php';
 
             <div class="col-md-9">
                 <h4>Results</h4>
-                <p>Page <?php echo $page; ?> of <?php echo $totalPages; ?></p>
-                <p> <?php echo $totalEntries; ?> entries match your search criteria.</p>
+                <p>Page
+                    <?php echo $page; ?> of
+                    <?php echo $totalPages; ?>
+                </p>
+                <p>
+                    <?php echo $totalEntries; ?> entries match your search criteria.
+                </p>
                 <?php
 
                 $type = $_GET['type'] ?? null;
@@ -91,7 +107,7 @@ require_once '../inc/functions.php';
 
                 <nav aria-label="Page navigation">
                     <ul class="pagination">
-                        <?php if ($page > 1) : ?>
+                        <?php if ($page > 1): ?>
                             <li class="page-item">
                                 <a class="page-link" href="?page=<?php echo $page - 1 . $type_text;
                                 ?>
@@ -105,38 +121,42 @@ require_once '../inc/functions.php';
                         $startPage = max(1, $page - 2);
                         $endPage = min($startPage + 4, $totalPages);
 
-                        if ($startPage > 1) :
-                        ?>
+                        if ($startPage > 1):
+                            ?>
                             <li class="page-item">
-                                <a class="page-link" href="?page=1<?php echo $type_text;?>">1</a>
+                                <a class="page-link" href="?page=1<?php echo $type_text; ?>">1</a>
                             </li>
-                            <?php if ($startPage > 2) : ?>
+                            <?php if ($startPage > 2): ?>
                                 <li class="page-item disabled">
                                     <a class="page-link">...</a>
                                 </li>
                             <?php endif; ?>
                         <?php endif; ?>
 
-                        <?php for ($i = $startPage; $i <= $endPage; $i++) : ?>
+                        <?php for ($i = $startPage; $i <= $endPage; $i++): ?>
                             <li class="page-item <?php echo ($i == $page) ? 'active' : ''; ?>">
-                                <a class="page-link" href="?page=<?php echo $i . $type_text; 
-                                
-                                ?>"><?php echo $i; ?></a>
+                                <a class="page-link" href="?page=<?php echo $i . $type_text;
+
+                                ?>">
+                                    <?php echo $i; ?>
+                                </a>
                             </li>
                         <?php endfor; ?>
 
-                        <?php if ($endPage < $totalPages) : ?>
-                            <?php if ($endPage < $totalPages - 1) : ?>
+                        <?php if ($endPage < $totalPages): ?>
+                            <?php if ($endPage < $totalPages - 1): ?>
                                 <li class="page-item disabled">
                                     <a class="page-link">...</a>
                                 </li>
                             <?php endif; ?>
                             <li class="page-item">
-                                <a class="page-link" href="?page=<?php echo $totalPages . $type_text;  ?>"><?php echo $totalPages; ?></a>
+                                <a class="page-link" href="?page=<?php echo $totalPages . $type_text; ?>">
+                                    <?php echo $totalPages; ?>
+                                </a>
                             </li>
                         <?php endif; ?>
 
-                        <?php if ($page < $totalPages) : ?>
+                        <?php if ($page < $totalPages): ?>
                             <li class="page-item">
                                 <a class="page-link" href="?page=<?php echo $page + 1 . $type_text; ?>
 
@@ -148,7 +168,7 @@ require_once '../inc/functions.php';
                     </ul>
                 </nav>
 
-                <?php foreach ($results as $result) : ?>
+                <?php foreach ($results as $result): ?>
                     <?php
                     $id = $result['id'];
                     $public = $result['public'];
@@ -166,7 +186,7 @@ require_once '../inc/functions.php';
                     $audio = $result['audio'] ?? null;
                     $image = $result['image'] ?? null;
                     ?>
-                    <?php if ($type == "text_only") : ?>
+                    <?php if ($type == "text_only"): ?>
                         <div class="card mb-3">
                             <div class="row g-0">
                                 <div class="col-md-12">
@@ -185,9 +205,11 @@ require_once '../inc/functions.php';
                                             </small></p>
 
 
-                                        <?php if ($type == "audio_only") : ?>
+                                        <?php if ($type == "audio_only"): ?>
                                             <div class="audio-player">
-                                                <h5> File: <?php echo $audio; ?> </h5>
+                                                <h5> File:
+                                                    <?php echo $audio; ?>
+                                                </h5>
                                                 <audio controls>
                                                     <source src=" <?php echo $audio; ?>
                                                 " type="audio/mpeg">
@@ -204,7 +226,7 @@ require_once '../inc/functions.php';
                         </div>
                     <?php endif; ?>
 
-                    <?php if ($type == "photo_and_text") : ?>
+                    <?php if ($type == "photo_and_text"): ?>
                         <div class="card mb-3">
                             <div class="row g-0">
                                 <div class="col-md-8">
@@ -222,13 +244,15 @@ require_once '../inc/functions.php';
                                     </div>
                                 </div>
                                 <div class="col-md-3 mx-auto d-flex align-items-center">
-                                    <img alt="<?php echo $image; ?>" src="assets/content/images-fe/<?php echo strip_tags($image); ?>" class="img-fluid rounded-start" alt="...">
+                                    <img alt="<?php echo $image; ?>"
+                                        src="assets/content/images-fe/<?php echo strip_tags($image); ?>"
+                                        class="img-fluid rounded-start" alt="...">
                                 </div>
                             </div>
                         </div>
                     <?php endif; ?>
 
-                    <?php if ($type == "audio_only") : ?>
+                    <?php if ($type == "audio_only"): ?>
 
                         <div class="card mb-3">
                             <div class="row g-0">
@@ -251,7 +275,8 @@ require_once '../inc/functions.php';
                                 <div class="col-md-4 mx-auto d-flex align-items-center">
                                     <div class="audio-player">
                                         <audio controls>
-                                            <source src="/assets/content/audio-fe/<?php echo strip_tags($audio); ?>" type="audio/mpeg">
+                                            <source src="/assets/content/audio-fe/<?php echo strip_tags($audio); ?>"
+                                                type="audio/mpeg">
                                             Your browser does not support the audio element.
                                         </audio>
                                     </div>
@@ -275,7 +300,20 @@ require_once '../inc/functions.php';
 
 
     <!-- Bootstrap 5 JS CDN (required for some components) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+    <script>
+        // Send the height of the content to the parent window
+        function sendHeight() {
+            var height = document.body.scrollHeight;
+            parent.postMessage({ height: height }, '*');
+        }
+
+        // Call sendHeight whenever the content changes (e.g., on load or after dynamic changes)
+        window.onload = sendHeight;
+        // You might need to call sendHeight on other events, depending on your content
+    </script>
+
 </body>
 
 </html>
