7109149b87dda93f8d2739dc594319e5f3fc3037
Joel Salisbury <joel.salisbury@gmail.com>
2023-12-21 21:00:07 -0500

    modal at top

--- views/layout-entry.php
+++ views/layout-entry.php
@@ -116,7 +116,7 @@
 <?php endif; ?>
 
 <div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-md">
+    <div class="modal-dialog modal-md">
         <div class="modal-content">
             <div class="modal-body">
             </div>
