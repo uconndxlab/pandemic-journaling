d8ad8915c1eb41e26d99b038a3ea6c08720b95f0
Joel Salisbury <joel.salisbury@gmail.com>
2024-01-16 15:31:20 -0500

    some uconn branding stuff.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
 require_once '../inc/functions.php';
+
+// check to see if it's being loaded in an iframe
+if (isset($_SERVER['HTTP_SEC_FETCH_DEST']) && $_SERVER['HTTP_SEC_FETCH_DEST'] == 'iframe') {
+    $isIframe = true;
+} else {
+    $isIframe = false;
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,11 +20,67 @@ require_once '../inc/functions.php';
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" crossorigin="anonymous">
     <!-- Font Awesome 5 CDN (required for some components) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" crossorigin="anonymous" />
+    <link rel="stylesheet" href="assets/css/banner.css">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
 <body>
-    <!-- sidebar -->
+    <?php if (!$isIframe) : ?>
+        <div id="uconn-banner">
+            <div id="uconn-header-container" class="row-container row-fluid">
+                <div id="home-link-container">
+                    <a id="home-link" href="https://uconn.edu/">
+                        <span id="wordmark" aria-hidden="true">UConn</span>
+                        <span id="university-of-connecticut" style="text-transform: uppercase;">
+                            University of Connecticut
+                        </span>
+                    </a>
+                </div>
+                <div id="button-container">
+                    <div class="icon-container" id="icon-container-search">
+                        <a class="btn" id="uconn-search" href="https://uconn.edu/search">
+                            <span class="no-css">Search University of Connecticut</span>
+                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 32 32" aria-hidden="true" class="banner-icon">
+                                <title>Search UConn</title>
+                                <path d="M28.072 24.749l-6.046-6.046c0.912-1.499 1.437-3.256 1.437-5.139 0-5.466-4.738-10.203-10.205-10.203-5.466 0-9.898 4.432-9.898 9.898 0 5.467 4.736 10.205 10.203 10.205 1.818 0 3.52-0.493 4.984-1.349l6.078 6.080c0.597 0.595 1.56 0.595 2.154 0l1.509-1.507c0.594-0.595 0.378-1.344-0.216-1.938zM6.406 13.258c0-3.784 3.067-6.853 6.851-6.853 3.786 0 7.158 3.373 7.158 7.158s-3.067 6.853-6.853 6.853-7.157-3.373-7.157-7.158z"></path>
+                            </svg>
+                        </a>
+                        <div id="uconn-search-tooltip"></div>
+                    </div>
+                    <div class="icon-container" id="icon-container-az">
+                        <a class="btn" id="uconn-az" href="https://uconn.edu/az">
+                            <span class="no-css">A to Z Index</span>
+                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 32 32" aria-hidden="true" class="banner-icon">
+                                <title>UConn A to Z Search</title>
+                                <path d="M5.345 8.989h3.304l4.944 13.974h-3.167l-0.923-2.873h-5.147l-0.946 2.873h-3.055l4.989-13.974zM5.152 17.682h3.579l-1.764-5.499-1.815 5.499zM13.966 14.696h5.288v2.56h-5.288v-2.56zM20.848 20.496l7.147-9.032h-6.967v-2.474h10.597v2.341l-7.244 9.165h7.262v2.466h-10.798v-2.466h0.004z"></path>
+                            </svg>
+                        </a>
+                        <div id="uconn-az-tooltip"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <nav class="upper-nav">
+            <div class="container">
+                <a class="parent-title" href="https://core.uconn.edu/news"></a>
+            </div>
+
+        </nav>
+
+        <nav class="navbar navbar-expand-lg navbar-light shift">
+            <div class="container">
+                <a class="navbar-brand ps-0" href="/">Pandemic Journaling Project Featured Entries</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+            </div>
+
+        </nav>
+    <?php endif; ?>
+
     <div class="container">
         <?php
 
@@ -78,15 +141,8 @@ require_once '../inc/functions.php';
 
                             <h6 class="form-label mt-4 mb-3" id="formatlabel">Text Search: </h6>
                             <div class="input-group mb-3">
-                                <input autocomplete="one-time-code" 
-                                id="search" 
-                                value="<?php if (isset($_GET['search'])) echo $_GET['search'];  else "" ?>" 
-                                type="text" 
-                                class="form-control" 
-                                placeholder="Search" 
-                                aria-label="Search" 
-                                aria-describedby="search-btn" 
-                                name="search">
+                                <input autocomplete="one-time-code" id="search" value="<?php if (isset($_GET['search'])) echo $_GET['search'];
+                                                                                        else "" ?>" type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="search-btn" name="search">
                             </div>
 
                             <h6 class="form-label mb-3" id="formatlabel">Format:</h6>
@@ -200,7 +256,7 @@ require_once '../inc/functions.php';
                         <ul class="pagination">
                             <?php if ($page > 1) : ?>
                                 <li class="page-item">
-                                    <a class="page-link" href="?page=<?php echo $page - 1 . $type_query . $lang_query . $search_query ; ?>
+                                    <a class="page-link" href="?page=<?php echo $page - 1 . $type_query . $lang_query . $search_query; ?>
 
                                 " aria-label="Previous">
                                         <span aria-hidden="true">Previous Page</span>
@@ -215,7 +271,7 @@ require_once '../inc/functions.php';
                             if ($startPage > 1) :
                             ?>
                                 <li class="page-item">
-                                    <a class="page-link" href="?page=1<?php echo $type_query . $lang_query . $search_query ; ?>">1</a>
+                                    <a class="page-link" href="?page=1<?php echo $type_query . $lang_query . $search_query; ?>">1</a>
                                 </li>
                                 <?php if ($startPage > 2) : ?>
                                     <li class="page-item disabled">
