e8228948d122e2d8da2091699ac62232d83f481b
Joel Salisbury <joel.saisbury@gmail.com>
2025-05-01 12:51:33 -0400

    layout-entry

--- views/layout-entry.php
+++ views/layout-entry.php
@@ -3,14 +3,12 @@
         <div class="row g-0">
             <div class="col-md-12">
                 <div class="card-body mt-3">
-                    <h5><span class="badge bg-warning text-dark">
-                            Text Only
-                        </span></h5>
+                    <h5><span class="badge bg-warning text-dark">Text Only</span></h5>
                     <h6 class="card-title mt-3 mb-2">
-                        <?php echo $subject; ?>
+                        <?= htmlspecialchars($subject, ENT_QUOTES, 'UTF-8') ?>
                     </h6>
                     <p class="card-text">
-                        <?php echo $excerpt_or_original_text; ?>
+                        <?= htmlspecialchars($excerpt_or_original_text, ENT_QUOTES, 'UTF-8') ?>
                     </p>
                 </div>
             </div>
@@ -18,13 +16,13 @@
         <div class="row ps-5 pe-5">
             <div class="col-md-12 d-flex">
                 <p class="card-text mb-3"><small class="text-muted">
-                        <?php echo $featured_at; ?>
+                        <?= htmlspecialchars($featured_at, ENT_QUOTES, 'UTF-8') ?>
                     </small></p>
                 <p class="card-text mb-3 ms-auto"><small class="text-muted">
-                        <!-- button that says share as a text link -->
-                        <a href="?entryID=<?php echo $id; ?>" target="_blank" class="btn btn-text">
+                        <a href="?entryID=<?= htmlspecialchars($id, ENT_QUOTES, 'UTF-8') ?>" target="_blank" class="btn btn-text">
                             <i class="fas fa-share"></i>
-                            Direct Link</a>
+                            Direct Link
+                        </a>
                     </small></p>
             </div>
         </div>
@@ -38,30 +36,29 @@
                 <div class="card-body mt-3">
                     <h5><span class="badge bg-warning text-dark">Text & Image</span></h5>
                     <h6 class="card-title mt-3 mb-2">
-                        <?php echo $subject; ?>
+                        <?= htmlspecialchars($subject, ENT_QUOTES, 'UTF-8') ?>
                     </h6>
                     <p class="card-text mb-4">
-                        <?php echo $excerpt_or_original_text; ?>
+                        <?= htmlspecialchars($excerpt_or_original_text, ENT_QUOTES, 'UTF-8') ?>
                     </p>
                 </div>
             </div>
             <div class="col-md-3 mx-auto d-flex align-items-center my-4">
-                <a href="assets/content/images-fe/<?php echo $image;?>"
-                target="_blank">
-                    <img alt="<?php echo $image; ?>" src="assets/content/images-fe/<?php echo strip_tags($image); ?>" class="img-fluid rounded-start" alt="...">
+                <a href="assets/content/images-fe/<?= htmlspecialchars($image, ENT_QUOTES, 'UTF-8') ?>" target="_blank">
+                    <img alt="<?= htmlspecialchars($image, ENT_QUOTES, 'UTF-8') ?>" src="assets/content/images-fe/<?= htmlspecialchars($image, ENT_QUOTES, 'UTF-8') ?>" class="img-fluid rounded-start">
                 </a>
             </div>
         </div>
         <div class="row ps-5 pe-5">
             <div class="col-md-12 d-flex">
                 <p class="card-text mb-3"><small class="text-muted">
-                        <?php echo $featured_at; ?>
+                        <?= htmlspecialchars($featured_at, ENT_QUOTES, 'UTF-8') ?>
                     </small></p>
                 <p class="card-text mb-3 ms-auto"><small class="text-muted">
-                        <!-- button that says share as a text link -->
-                        <a href="?entryID=<?php echo $id; ?>" target="_blank" class="btn btn-text">
+                        <a href="?entryID=<?= htmlspecialchars($id, ENT_QUOTES, 'UTF-8') ?>" target="_blank" class="btn btn-text">
                             <i class="fas fa-share"></i>
-                            Direct Link</a>
+                            Direct Link
+                        </a>
                     </small></p>
             </div>
         </div>
@@ -69,21 +66,20 @@
 <?php endif; ?>
 
 <?php if ($type == "audio_only") : ?>
-
     <div class="card mb-3">
         <div class="row g-0">
             <div class="col-md-7">
                 <div class="card-body mt-3">
                     <h5><span class="badge bg-warning text-dark">Text & Audio</span></h5>
                     <h6 class="card-title mt-3 mb-2">
-                        <?php echo $subject; ?>
+                        <?= htmlspecialchars($subject, ENT_QUOTES, 'UTF-8') ?>
                     </h6>
                     <div class="card-text">
-                        <?php echo strip_tags($excerpt_or_original_text); ?>
+                        <?= htmlspecialchars($excerpt_or_original_text, ENT_QUOTES, 'UTF-8') ?>
                     </div>
                     <p class="card-text mb-3">
                         <small class="text-muted">
-                            <?php echo $featured_at; ?>
+                            <?= htmlspecialchars($featured_at, ENT_QUOTES, 'UTF-8') ?>
                         </small>
                     </p>
                 </div>
@@ -91,7 +87,7 @@
             <div class="col-md-4 mx-auto d-flex align-items-center">
                 <div class="audio-player">
                     <audio controls>
-                        <source src="/assets/content/audio-fe/<?php echo strip_tags($audio); ?>" type="audio/mpeg">
+                        <source src="/assets/content/audio-fe/<?= htmlspecialchars($audio, ENT_QUOTES, 'UTF-8') ?>" type="audio/mpeg">
                         Your browser does not support the audio element.
                     </audio>
                 </div>
@@ -100,16 +96,13 @@
         <div class="row ps-5 pe-5">
             <div class="col-md-12 d-flex">
                 <p class="card-text mb-3"><small class="text-muted">
-                        <?php echo $featured_at; ?>
+                        <?= htmlspecialchars($featured_at, ENT_QUOTES, 'UTF-8') ?>
                     </small></p>
                 <p class="card-text mb-3 ms-auto"><small class="text-muted">
-                        <!-- button that says share as a text link -->
-                        <a href="?entryID=<?php echo $id; ?>" target="_blank" class="btn btn-text">
+                        <a href="?entryID=<?= htmlspecialchars($id, ENT_QUOTES, 'UTF-8') ?>" target="_blank" class="btn btn-text">
                             <i class="fas fa-share"></i>
-                            Direct Link</a>
-
-
-
+                            Direct Link
+                        </a>
                     </small></p>
             </div>
         </div>
@@ -127,11 +120,3 @@
         </div>
     </div>
 </div>
-
-<script>
-    function showImage(image) {
-        //document.querySelector('.modal-body').innerHTML = `<img src="assets/content/images-fe/${image}" class="img-fluid rounded-start" alt="...">`;
-    }
-
-</script>
-
